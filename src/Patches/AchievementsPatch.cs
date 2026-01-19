a54615cb3fac63e57b95164f6fa6218401deecfc
seadoggie001 <seaOfTheDogs@gmail.com>
2026-01-19 06:20:05 -0500

    fix: Achievement tracking only if enabled
    Also submits achievements to the AP server now

--- src/Patches/AchievementsPatch.cs
+++ src/Patches/AchievementsPatch.cs
@@ -13,13 +13,14 @@ public class AchievementsPatch
     [HarmonyPrefix]
     public static void UnlockAchievement(string achievement)
     {
+        if (!Plugin.Instance.Enabled) return;
+        Achievements.enabled = false;
+        // ToDo: Lock object might only be needed when dealing with Steam Achievements... need to check on this. 
         lock (LockObject)
         {
             if(!UnlockedAchievements.Add(achievement)) return;
             switch (achievement)
             {
-                case "CAUSE_A_RUNTIME_ERROR":
-                    MainSim.Inst.UnlockHat(ResourceManager.GetHat(TFWRTypes.Hat.TrafficCone.Resource));
                 case Achievement.CauseARuntimeError:
                     MainSim.Inst.UnlockHat(ResourceManager.GetHat(Constants.Hat.TrafficCone.Resource));
                     break;
@@ -30,8 +31,12 @@ public class AchievementsPatch
                     MainSim.Inst.UnlockHat(ResourceManager.GetHat(Constants.Hat.Wizard.Resource));
                     break;
             }
-            // Do archipelago stuff here
-            if(!Plugin.Instance.Enabled) return;
+            // Convert TFWR achievement into AP location name
+            string locationName = Unlocks.AchievementToLocation(achievement);
+            // AP Location name to ID
+            long locationId = Plugin.Instance.Session.Locations.GetLocationIdFromName(Plugin.GameName, locationName);
+            // Send the location to the server
+            Plugin.Instance.Session.Locations.CompleteLocationChecks(locationId);
         }
     }
 
@@ -39,6 +44,7 @@ public class AchievementsPatch
     [HarmonyPrefix]
     public static void IncrementStat(string statName, int increment)
     {
+        if (!Plugin.Instance.Enabled) return;
         UserStats.Add(statName, increment);
     }
 }
