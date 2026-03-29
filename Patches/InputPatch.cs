24e92a4d49ffea31c9d1a87642089c1eb47da366
seadoggie001 <seaOfTheDogs@gmail.com>
2026-03-29 08:28:37 -0400

    fix: AP Connection Settings dialog
    - Enable Dragging (finally!)
    - Prevent UI clicks from passing through the dialog (InputPatch.cs)
    - Reformatting

---
+++ Patches/InputPatch.cs
@@ -0,0 +1,24 @@
+using com.seadoggie.TFWRArchipelago.Configuration;
+using HarmonyLib;
+using UnityEngine;
+
+namespace com.seadoggie.TFWRArchipelago.Patches;
+
+[HarmonyPatch(typeof(Input))]
+public class InputPatch
+{
+    /// <summary>
+    /// This prevents mouse clicks when the IMGUI connection screen is visible
+    /// </summary>
+    /// <param name="__result"></param>
+    /// <returns></returns>
+    [HarmonyPatch(typeof(Input), nameof(Input.GetMouseButtonDown))]
+    [HarmonyPrefix]
+    public static bool GetMouseButtonDown(ref bool __result) {
+        // If the connection screen is not visible or the mouse is outside the window, process normally
+        if (!(ArchipelagoSettings.Instance?.DisplayingWindow ?? false) || !ArchipelagoSettings.Instance.IsMouseOverWindow()) return !Plugin.HarmonySkipFunction;
+        // the mouse click was handled. Skip the rest of the function.
+        __result = false;
+        return Plugin.HarmonySkipFunction;
+    }
+}
