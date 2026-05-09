d2952776c415b12cf4eb47ad0ba5f198da7f91bd
seadoggie001 <seaOfTheDogs@gmail.com>
2026-05-09 09:50:02 -0400

    fix: Use a default version for the mod save game

--- ModSaveGame.cs
+++ ModSaveGame.cs
@@ -2,6 +2,6 @@ namespace com.seadoggie.TFWRArchipelago;
 
 public class ModSaveGame
 {
-    public string Version;
+    public string Version = "1";
     public List<Pair<string, double>> Statistics;
 }
